30137291a1b82c9118c2870199890ae962121265
Somaur <1376129425@qq.com>
2022-11-14 18:45:07 +0800

    22-11-14 临时框架1

---
+++ app/src/main/java/com/example/pesticide_pass/AddModelActivity.java
@@ -0,0 +1,14 @@
+package com.example.pesticide_pass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddModelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_model);
+    }
+}
