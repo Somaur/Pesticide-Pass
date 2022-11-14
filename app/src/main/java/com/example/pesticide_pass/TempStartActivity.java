3fdaf83dd1091ab455e3866b6c25a80059f746e9
Somaur <1376129425@qq.com>
2022-11-14 18:40:16 +0800

    22-11-14 临时框架1

---
+++ app/src/main/java/com/example/pesticide_pass/TempStartActivity.java
@@ -0,0 +1,64 @@
+package com.example.pesticide_pass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TempStartActivity extends AppCompatActivity {
+
+    Button button1; // 添加
+    Button button2; // 预测
+    Button button3; // 上传
+    Button button4; // 管理
+    Button button5; // 登录
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_start);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        setButtonsListener();
+
+    }
+
+    private void setButtonsListener(){
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TempStartActivity.this, AddModelActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
